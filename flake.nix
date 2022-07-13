8936328e699341fb8eb582103377c2fa50f8dae5
Pierre Bourdon <delroth@gmail.com>
2022-07-13 14:32:35 +0200

    Increase flakiness

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A simple JavaScript page to locate bikes at close-by publibike.ch stations in Switzerland";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      packages.publibike-locator = with pkgs; stdenv.mkDerivation {
+        name = "publibike-locator";
+
+        src = lib.cleanSource ./.;
+
+        nativeBuildInputs = [ nodePackages.typescript ];
+
+        installPhase = ''
+          mkdir $out
+          cp index.html app.js manifest.json worker.js favicon.svg $out
+        '';
+
+        meta = with lib; {
+          homepage = https://delroth.net/publibike/;
+          license = with licenses; [ mit ];
+          maintainers = with maintainers; [ delroth ];
+        };
+      };
+
+      defaultPackage = packages.publibike-locator;
+
+      devShells.default = with pkgs; mkShell {
+        packages = [ gnumake inkscape ];
+        inputsFrom = [ defaultPackage ];
+      };
+    });
+}
